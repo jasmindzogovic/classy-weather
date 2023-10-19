49065c58f8b98ded30ea8405ee1722a0a45a5e8b
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-19 15:20:28 -0400

    removed unneccessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this)
+  }
+  handleDecrement() {
+    console.log(this)
+    // this.setState(s=>s-1);
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
